e31ec0da5353c1c470efb34566eaf2792e7f9bce
Amir Zigangaraev <amir.zigangaraev@gmail.com>
2020-09-14 21:02:45 +0300

    Add HomeWorkController

---
+++ MyFirstProject/MyFirstProject/HomeWorkController.swift
@@ -0,0 +1,12 @@
+//
+//  HomeWorkController.swift
+//  MyFirstProject
+//
+//  Created by Amir Zigangaraev on 14.09.2020.
+//  Copyright © 2020 Amir Zigangaraev. All rights reserved.
+//
+
+import UIKit
+
+class HomeWorkController: UIViewController {
+}
