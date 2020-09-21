cdbd8250a470f256028373d8e4afae0206a799ef
evelina <yangirova01@bk.ru>
2020-09-21 10:44:55 -0700

    Hello world

---
+++ MyFirstProject/Homework/StatusView.swift
@@ -0,0 +1,13 @@
+//
+//  StatusView.swift
+//  Homework
+//
+//  Created by Эвелина on 20.09.2020.
+//  Copyright © 2020 Amir Zigangaraev. All rights reserved.
+//
+
+import UIKit
+
+class StatusView: UIView {
+
+}
